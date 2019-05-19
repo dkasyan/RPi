738bcb2c910286f846ae12c19290bc270db3348e
Damian Kasyan <damian.kasyan@hotmail.com>
2019-05-19 16:28:00 +0200

    Update dht11.py
    Loop for parm

--- dht11.py
+++ dht11.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 
-print "Hello";
+import Adafruit_DHT
+
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 27)  # GPIO27 (BCM notation)
+    print ("Humidity = {} %; Temperature = {} C".format(humidity, temperature))
